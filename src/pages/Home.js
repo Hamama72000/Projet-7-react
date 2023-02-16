28495e7ccaf1aa0a3e87bcf3ccfbbdb9a7fe8961
Hamama Assadi <hmima495gmail.com>
2023-02-16 19:13:02 +0100

    image bannière page Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
+import React from "react";
 import Banner from '../components/Banner.js'
+import BGHome from "../assets/home_bg_banner.png"
 
-function Home() {
-    return <Banner />
+
+
+export default function Home() {
+ 
+    document.title = "Kasa - Accueil";
+    return (
+        <main className="display_home">
+            <Banner img={BGHome} text1={'Chez vous,'} text2={'partout et ailleurs'} />
+            <div className="display_card">
+            </div>
+        </main>
+    );
 }
-
-export default Home
-
