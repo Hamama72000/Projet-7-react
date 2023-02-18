b809174dbc09202b27e11bf7b6ac6a66170344f7
Hamama Assadi <hmima495gmail.com>
2023-02-18 14:47:53 +0100

    card

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "../styles/Navbar.css";
-import logo from "../assets/logo.png";
+import Logo from "../assets/logo.png";
 
-const navbar 
+
+export default function navbar() {
+    return (
+        <header>
+                <img src={Logo} alt="logo kasa" className="logo" />
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink
+                            to="/"
+                            end
+                            className={({ isActive }) => //sert à souligner le texte si je suis sur la page d'accueil
+                                isActive
+                                    ? "accueil_navbar active"
+                                    : "accueil_navbar"
+                            }
+                        >
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "about_navbar active"
+                                    : "about_navbar"
+                            }
+                        >
+                            A Propos
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,22 @@
 import React from "react";
-import Banner from '../components/Banner.js'
-import BGHome from "../assets/home_bg_banner.png"
-     
+import Banner from "../components/Banner.js";
+import BGHome from "../assets/home_bg_banner.png";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+import Properties from "../data/logements";
 
 export default function Home() {
- 
-    document.title = "Kasa - Accueil";
-    return (
-        <main className="display_home">
-            <Banner img={BGHome} text1={'Chez vous, partout et ailleurs'}  />
-            <div className="display_card">
-            </div>
-        </main>
-    );
+  document.title = "Kasa - Accueil";
+
+  return (
+    <main className="display_home">
+      <Navbar />
+      <Banner img={BGHome} text1={"Chez vous, partout et ailleurs"} />
+      <div className="display_card">
+        {Properties.map((logement) => (
+          <Card key={logement.id} property={logement} />
+        ))}
+      </div>
+    </main>
+  );
 }
