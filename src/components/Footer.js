d8448d787787f37a0c134434642e9c5a971cc6e0
Hamama Assadi <hmima495gmail.com>
2023-02-21 15:22:57 +0100

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../assets/logo_footer.png';
+import '../styles/footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <img src={Logo} alt="logo kasa footer" />
+        <p>
+          <i className="fa-regular fa-copyright"></i>
+          2020 Kasa. All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
