675479ee408d524f6ec981caee427a1774075e67
Hamama Assadi <hmima495gmail.com>
2023-02-27 17:42:51 +0100

    format doc

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,17 +2,16 @@ import React from "react";
 import "../styles/Banner.css";
 import maskBG from "../assets/maskBG.png";
 
-
-const Banner =  ({ img, text1 }) => {
-        return (
-            <div className="banner_container">
-                <img src={img} alt="" className="imgBG" />
-                <img src={maskBG} alt="filtre bannière" className="maskBG" />
-                <div className="banner_text">
-                    <p>{text1}</p>
-                </div>
-            </div>
-        );
-    };
+const Banner = ({ img, text1 }) => {
+  return (
+    <div className="banner_container">
+      <img src={img} alt="" className="imgBG" />
+      <img src={maskBG} alt="filtre bannière" className="maskBG" />
+      <div className="banner_text">
+        <p>{text1}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
