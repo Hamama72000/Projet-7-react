cb8657a23e2567b41157d7e974b726738965b310
Hamama Assadi <hmima495gmail.com>
2023-02-27 16:10:47 +0100

    index

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import "./styles/App.css";
 import Home from "./pages/Home.js";
 import About from "./pages/About";
 import Logement from "./pages/Logement";
@@ -8,24 +9,23 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <>
-        <Navbar />
-            <Routes>
-                    {/* Page par default */}
-                    <Route index element={<Home />} />
-                    {/* Routage Page */}
-                    <Route path="/home" element={<Home />} />
-                    <Route path={`/logement/:id`} element={<Logement />} />
-                    <Route path="/about" element={<About />} />
-                    {/* Erreur URL */}
-                    <Route path="*" element={<NotFound />} />
-                    <Route path="/404" element={<NotFound />} />
-           
-            </Routes>
-        <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        {/* Page par default */}
+        <Route index element={<Home />} />
+        {/* Routage Page */}
+        <Route path="/home" element={<Home />} />
+        <Route path={`/logement/:id`} element={<Logement />} />
+        <Route path="/about" element={<About />} />
+        {/* Erreur URL */}
+        <Route path="*" element={<NotFound />} />
+        <Route path="/404" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
